7675c9a44a96164dfac3de20cccc7f598ee37287
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-06 15:20:28 +0900

    initial setting

---
+++ VideoPlayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayApp
+//
+//  Created by 유림 on 5/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
