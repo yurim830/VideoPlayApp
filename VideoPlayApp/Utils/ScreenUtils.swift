e240890c6bc66de911c90930139c14a7377cfdd9
Yurim Kim <yurim830@gmail.com>
2025-11-14 18:29:53 +0900

    [Add,Chore] ScreenUtils 구현, Then 파일경로 누락부분 추가 (#1)

---
+++ VideoPlayApp/Utils/ScreenUtils.swift
@@ -0,0 +1,15 @@
+//
+//  ScreenUtils.swift
+//  VideoPlayApp
+//
+//  Created by 김유림 on 11/14/25.
+//
+
+import UIKit
+
+struct ScreenUtils {
+    
+    static let width: CGFloat = UIScreen.main.bounds.width
+    static let height: CGFloat = UIScreen.main.bounds.height
+    
+}
